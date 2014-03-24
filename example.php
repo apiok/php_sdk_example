b1394b6c156350d645089f358118faa0aa2f0f3b
artyom.lobachev <artyom.lobachev@odnoklassniki.ru>
2014-03-24 15:36:09 +0400

    Added english translation of comments

--- example.php
+++ example.php
@@ -9,6 +9,10 @@
     <body>
         <?php
         /*
+        You have to add this code once before first usage of SDK. Change path to YOUR SDK path in require directive. 
+        It connects SDK and checks curl lib is installed.
+        */
+        /*
         Этот код необходимо добавить один раз перед первым использованием SDK, указав в директиве require путь к файлу с SDK.
         Он подключает SDK и проверяет, установлена ли необходимая для работы SDK библиотека curl.
         */
@@ -20,18 +24,27 @@
         ?>
         <?php
         /*
+        This is example of using SDK.
+        */
+        /*
         Это пример использования SDK.
         */
         $template = "<div id=\"t\"><div id=\"tr\"><div class=\"tc\" id=\"tc1\"><img src=\"%s\" class=\"pic\" alt=\"user photo\">%s</div><div class=\"tc\" id=\"tc2\">дружит с</div><div class=\"tc\" id=\"tc3\"><img src=\"%s\" class=\"pic\" alt=\"user photo\">%s</div></div></div>";
+        // method checks if request has parameter code
         // если в запросе есть параметр code (считается, что параметр получен после авторизации пользователя на ok)
         if (!is_null(OdnoklassnikiSDK::getCode())){
             if(OdnoklassnikiSDK::changeCodeToToken(OdnoklassnikiSDK::getCode())){
+                // example of using makeRequest with parameters
+                // method returns required info about current user
                 // пример вызова метода с параметрами
                 // запрашиваем информацию о текущем пользователе
                 $current_user = OdnoklassnikiSDK::makeRequest("users.getCurrentUser", array("fields" => "name,pic_5"));
+                // example of using makeRequest without parameters
+                // method returns list of user's friends
                 // пример вызова метода без параметров
                 // запрашиваем списко друзей пользователя
                 $friends = OdnoklassnikiSDK::makeRequest("friends.get");
+                // method returns required info about required users
                 // запрашиваем имя и ссылку на фото первого друга из списка
                 $first_friend = OdnoklassnikiSDK::makeRequest("users.getInfo", array("fields" => "name,pic_5", "uids" => $friends[0]))[0];
                 printf($template, $current_user["pic_5"], $current_user["name"], $first_friend["pic_5"], $first_friend["name"]);
